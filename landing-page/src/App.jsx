e18ae1342f12be292280a02927e6cbbeba22e570
Rutu-Koladiya <rutukoladiya@gmail.com>
2025-08-02 21:09:44 +0530

    add landing page

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "./components/HeroSection"
+import Services from "./components/Services"
+
+function App() {
+
+  return (
+    <>
+      <HeroSection />
+      <Services />
+    </>
+  )
+}
+
+export default App
