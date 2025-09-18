30562faf3307f473ff733bb4348a04a7b76e9df6
Rutu-Koladiya <rutukoladiya@gmail.com>
2025-09-18 18:18:16 +0530

    chnages in vtube

--- vtube-backend/src/utils/cloudinary.js
+++ vtube-backend/src/utils/cloudinary.js
@@ -1,17 +1,29 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+// Initialize Cloudinary only if environment variables are available
+if (!process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+  console.error("Missing Cloudinary credentials!");
+} else {
+  console.log("Cloudinary credentials loaded successfully!");
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const uploadOnCloudinary = (localFilePath) => {
+export const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
 
-    const response = cloudinary.uploader.upload(localFilePath, {
+    console.log(process.env.CLOUDINARY_CLOUD_NAME, "cloud name");
+    console.log(process.env.CLOUDINARY_API_KEY, "cloud api key");
+    console.log(process.env.CLOUDINARY_API_SECRET, "cloud api secreat");
+    console.log(localFilePath, "local file path");
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
 
@@ -23,6 +35,7 @@ export const uploadOnCloudinary = (localFilePath) => {
     // fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
+    console.error("Error uploading file to Cloudinary:", error);
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
